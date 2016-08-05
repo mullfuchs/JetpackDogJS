832c7d47bdfed284b78ca8478d83caf3ea70f6ab
mullfuchs <burke.leo@gmail.com>
2016-08-05 10:25:01 -0700

    commiting some changes to the webpage

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '#gameDiv');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
